15b0de994dddfd1892d113eadc6ef4960b47616b
= <=>
2016-04-26 08:33:40 -0700

    version2

--- fibonachi.rb
+++ fibonachi.rb
@@ -1,10 +1,8 @@
-def fibonachi(int)
-    @int = int
-    pred = 0
-    sled = 1
-    while sled <= @int do
-        sled = sled + pred
-        pred = sled - pred        
+def fibonachi(int, pred=0, sled=1)
+    @int = int; @pred = pred; @sled = sled    
+    while @sled <= @int do
+        @sled = @sled + @pred
+        @pred = @sled - @pred
     end
-    sled
+    @sled
 end
---
+++ fibonachi.rb~
@@ -0,0 +1,10 @@
+def fibonachi(int)
+    @int = int
+    pred = 0
+    sled = 1
+    while sled <= @int do
+        sled = sled + pred
+        pred = sled - pred        
+    end
+    sled
+end
